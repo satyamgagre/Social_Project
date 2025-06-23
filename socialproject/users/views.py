c99fdee3f8521555acfa2d65b2248565b0603a2e
Satyam Gagare <satyamgagre4@gmail.com>
2025-06-24 01:18:37 +0530

    .

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
-    form = LoginForm()
-    return render(request, 'users/login.html', {'form':form}) 
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("User authenticated and logged in.")
+            else:
+                return HttpResponse("Invalid credentials.")
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "You have been logged out successfully.")
+    return redirect('login')  
+
+@login_required
+def index(request):
+    return render(request,'users/index.html')
