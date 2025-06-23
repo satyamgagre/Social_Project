09d5d6f0c36c6a5cfaf41c83c9023c7c45f42158
Satyam Gagare <satyamgagre4@gmail.com>
2025-06-24 02:23:22 +0530

    .

--- socialproject/users/forms.py
+++ socialproject/users/forms.py
@@ -1,5 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+    # This method automatically validates password match
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd.get('password') != cd.get('password2'):
+            raise forms.ValidationError('Passwords do not match')
+        return cd.get('password2')
