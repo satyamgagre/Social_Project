34b043aa4ad0a4ec74db55b0acff72f5ef1a33d0
Satyam Gagare <satyamgagre4@gmail.com>
2025-06-27 04:14:37 +0530

    ,

--- socialproject/socialproject/settings.py
+++ socialproject/socialproject/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+# For Render database
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
