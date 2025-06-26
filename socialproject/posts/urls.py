2619238f4e312e2d02992abb0713a33c93655fc1
Satyam Gagare <satyamgagre4@gmail.com>
2025-06-26 18:23:18 +0530

    .

--- socialproject/posts/urls.py
+++ socialproject/posts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create',views.post_create, name="create"),
+    path('feed',views.feed, name="feed"),
+    path('like_post',views.like_post, name="like_post"),
 ]
--- socialproject/posts/views.py
+++ socialproject/posts/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from django.contrib import messages
+from .models import Post
 
 @login_required
 def post_create(request):
     if request.method == 'POST':
-        form = PostCreateForm(data=request.POST, files=request.FILES)
+        form = PostCreateForm(request.POST, request.FILES)
         if form.is_valid():
             new_item = form.save(commit=False)
             new_item.user = request.user
             new_item.save()
+            messages.success(request, '✅ Your post has been created successfully!')
+            return redirect('create')  
     else:
-        form = PostCreateForm(data=request.GET)
-    return render(request, 'posts/create.html',{'form':form})
+        form = PostCreateForm()
+    return render(request, 'posts/create.html', {'form': form})
+
+def feed(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/feed.html',{'posts':posts})
+
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(post, id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists():
+        post.liked_by.remove(request.user)
+    else:
+        post.liked_by.add(request.user)
+
+
